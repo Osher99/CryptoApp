c514c441badf37b8db707a0e5d2892b9eb55ce58
Osher Dror <osherd25@gmail.com>
2022-09-26 12:01:49 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,9 +34,9 @@ const Navbar = () => {
     return (
         <div className="nav-container">
             <div className="logo-container">
-                <Avatar src={icon} size="large" />}
+                <Avatar src={icon} size="large" />
                 <Typography.Title level={2} className="logo">
-                    <Link to="/" className="menu-logo-title">CryptoX {!isMobile && <><br/ ></>} <small style={{ fontSize: '11px' }}>By Osher Dror</small></Link>
+                    <Link to="/" className="menu-logo-title">CryptoX {isMobile && <><br/ ></>} <small style={{ fontSize: '11px' }}>By Osher Dror</small></Link>
                 </Typography.Title>
                 <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
                     <MenuOutlined />
