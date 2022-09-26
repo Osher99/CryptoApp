7e929b6a829317c0fb6941c8bc338c4866822e6a
Osher Dror <osherd25@gmail.com>
2022-09-27 00:07:38 +0300

    added transforemd navbar for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import {
     MoneyCollectOutlined,
     BulbOutlined,
     FundOutlined,
-    MenuOutlined
+    MenuFoldOutlined,
+    MenuUnfoldOutlined
 } from '@ant-design/icons';
 import icon from '../assets/cryptocurrency.png';
 
@@ -14,6 +15,8 @@ const Navbar = () => {
     const [activeMenu, setActiveMenu] = useState(true);
     const [screenSize, setScreenSize] = useState(true);
     const [isMobile, setIsMobile] = useState(false);
+    const [navClass, setNavClass] = useState('slide');
+
     useEffect(() => {
         const handleResize = () => setScreenSize(window.innerWidth);
         window.addEventListener('resize', handleResize);
@@ -24,27 +27,28 @@ const Navbar = () => {
     useEffect(() => {
         if (screenSize < 800) {
             setIsMobile(true);
-            setActiveMenu(false);
+            setActiveMenu(true);
         } else {
             setIsMobile(false);
-            setActiveMenu(true);
         }
-    },[screenSize])
+    }, [screenSize])
 
     return (
         <div className="nav-container">
             <div className="logo-container">
                 <Avatar src={icon} size="large" />
                 <Typography.Title level={2} className="logo">
-                    <Link to="/" className="menu-logo-title">CryptoX {!isMobile && <><br/ ></>} <small style={{ fontSize: '11px' }}>By Osher Dror</small></Link>
+                    <Link to="/" className="menu-logo-title">CryptoX {!isMobile && <><br /></>} <small style={{ fontSize: '11px' }}>By Osher Dror</small></Link>
                 </Typography.Title>
-                <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
-                    <MenuOutlined />
+                <Button className="menu-control-container" onClick={() => {
+                    setActiveMenu(!activeMenu);
+                    activeMenu ? setNavClass("") : setNavClass("slide");
+                }
+                }>
+                    {activeMenu ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
                 </Button>
-                </div>
-
-                {activeMenu && 
-                <Menu theme="dark" className="menu-items">
+            </div>
+                <Menu theme="dark" className={`menu-items ${navClass}`}>
                     <Menu.Item icon={<HomeOutlined />} key="homepage">
                         <Link to="/" onClick={() => isMobile ? setActiveMenu(false) : null}>Home</Link>
                     </Menu.Item>
@@ -54,11 +58,10 @@ const Navbar = () => {
                     <Menu.Item icon={<MoneyCollectOutlined />} key="exchanges">
                         <Link to="/exchanges" onClick={() => isMobile ? setActiveMenu(false) : null}>Ranking</Link>
                     </Menu.Item>
-                    <Menu.Item icon={<BulbOutlined />}  key="news">
+                    <Menu.Item icon={<BulbOutlined />} key="news">
                         <Link to="/news" onClick={() => isMobile ? setActiveMenu(false) : null}>News</Link>
                     </Menu.Item>
                 </Menu>
-                }
         </div>
     );
 };
