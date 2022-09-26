c4884af5ce4a8a70787f920f8b0c2ef7fec73fed
Osher Dror <osherd25@gmail.com>
2022-09-26 19:26:03 +0300

    multi[le fixes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -3,6 +3,7 @@ import millify from 'millify';
 import { Collapse, Row, Col, Typography, Avatar } from 'antd';
 import { useGetExchangesQuery } from '../services/cryptoApi';
 import MultipleLoading from './MultipleLoading';
+import icon from '../assets/expand.png';
 
 const { Text } = Typography;
 const { Panel } = Collapse;
@@ -23,28 +24,28 @@ const Exchanges = () => {
       </Row>
       <Row>
         {exchangesList?.map((exchange) => (
-          <Col span={24}>
+          <Col span={24} key={exchange.uuid}>
             <Collapse>
               <Panel
                 key={exchange.uuid}
                 showArrow={false}
                 header={(
-                  <Row key={exchange.uuid}>
+                  <Row key={exchange.uuid} className="header-exchange">
                     <Col span={6}>
-                      <Text><strong>{exchange.rank}.</strong></Text>
                       <Avatar className="exchange-image" src={exchange.iconUrl} />
+                      <Text><strong>{exchange.rank}.</strong></Text>
                       <Text><strong>{exchange.name}</strong></Text>
                     </Col>
                     <Col span={6}>${millify(exchange['24hVolume'])}</Col>
                     <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
-                    <Col span={6}>{millify(exchange.price)}</Col>
-                      </Row>
-                  )}
+                    <Col span={6}>{millify(exchange.price)}<Avatar className="expand-icon" src={icon} width="25px" height="25px" /></Col>
+                  </Row>
+                )}
               >
-                <Col span={6}>Ranking Url: <br /> 
-                <a href={exchange.coinrankingUrl} target="_blank" rel="noreferrer">
-                {exchange.coinrankingUrl}
-                </a>
+                <Col span={6}>
+                  <a href={exchange.coinrankingUrl} target="_blank" rel="noreferrer" style={{ overflowWrap: 'break-word' }}>
+                    Ranking URL
+                  </a>
                 </Col>
               </Panel>
             </Collapse>
